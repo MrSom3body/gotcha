e10ca77870f52725963b69332e11f24ae818fc5a
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-12 20:28:34 +0100

    install completions when using nix package

--- nix/package.nix
+++ nix/package.nix
@@ -16,4 +16,14 @@ buildGoModule rec {
     "-X 'github.com/MrSom3body/gotcha/cmd.version=v${version}'"
     "-extldflags '-static'"
   ];
+
+  postInstall = ''
+    mkdir -p $out/share/bash-completion/completions
+    mkdir -p $out/share/zsh/site-functions
+    mkdir -p $out/share/fish/vendor_completions.d
+
+    $out/bin/gotcha completion bash > $out/share/bash-completion/completions/gotcha
+    $out/bin/gotcha completion zsh > $out/share/zsh/site-functions/_gotcha
+    $out/bin/gotcha completion fish > $out/share/fish/vendor_completions.d/gotcha.fish
+  '';
 }
