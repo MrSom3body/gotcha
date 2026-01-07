f778740d57d7f0c8e53ee77969d93d2d1aa0b99f
Karun Sandhu <karun@sndh.dev>
2026-01-07 10:35:52 +0100

    make sure desktop name does not contain start-hyprland:

--- lib/desktop.go
+++ lib/desktop.go
@@ -3,10 +3,11 @@ package lib
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func GetDesktopEnvironment() string {
-	desktop := os.Getenv("XDG_CURRENT_DESKTOP")
+	desktop := strings.TrimPrefix(os.Getenv("XDG_CURRENT_DESKTOP"), "start-hyprland:")
 	session := os.Getenv("XDG_SESSION_TYPE")
 	return fmt.Sprintf("%s (%s)", desktop, session)
 }
