a076027009c8edcfa237972fc602dc86cfcd9506
Karun Sandhu <karun@sndh.dev>
2026-04-11 16:27:16 +0200

    desktop: no need to trim start-hyprland: anymore

--- lib/desktop.go
+++ lib/desktop.go
@@ -3,11 +3,10 @@ package lib
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func GetDesktopEnvironment() string {
-	desktop := strings.TrimPrefix(os.Getenv("XDG_CURRENT_DESKTOP"), "start-hyprland:")
+	desktop := os.Getenv("XDG_CURRENT_DESKTOP")
 	session := os.Getenv("XDG_SESSION_TYPE")
 	return fmt.Sprintf("%s (%s)", desktop, session)
 }
